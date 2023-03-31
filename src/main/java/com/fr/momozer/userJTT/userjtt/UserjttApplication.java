747d2bc405a31548042178e48228a048dc07108c
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-31 08:43:41 +0200

    fix : SecurityConfig class name fixed

--- src/main/java/com/fr/momozer/userJTT/userjtt/UserjttApplication.java
+++ src/main/java/com/fr/momozer/userJTT/userjtt/UserjttApplication.java
@@ -1,13 +1,41 @@
 package com.fr.momozer.userJTT.userjtt;
 
+import com.fr.momozer.userJTT.userjtt.model.Role;
+import com.fr.momozer.userJTT.userjtt.model.User;
+import com.fr.momozer.userJTT.userjtt.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserjttApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserjttApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService){
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null, "John Travolta", "John", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Will Smith", "will", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Luc skywalker", "Luc", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Clark Kent", "superman", "1234", new ArrayList<>()));
+
+			userService.addRoleToUser("john", "ROLE_USER");
+			userService.addRoleToUser("john", "ROLE_MANAGER");
+			userService.addRoleToUser("john", "ROLE_ADMIN");
+			userService.addRoleToUser("will", "ROLE_USER");
+			userService.addRoleToUser("superman", "ROLE_USER");
+		};
 	}
 
 }
---
+++ src/main/java/com/fr/momozer/userJTT/userjtt/security/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.fr.momozer.userJTT.userjtt.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration @EnableWebSecurity @RequiredArgsConstructor
+public class SecurityConfig{
+}
