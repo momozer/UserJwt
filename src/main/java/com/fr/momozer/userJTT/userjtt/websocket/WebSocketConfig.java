53e0a5d34f143acc6ec97156773e669ccce288c4
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-31 09:41:17 +0200

    feat : websockets classes addded

---
+++ src/main/java/com/fr/momozer/userJTT/userjtt/websocket/WebSocketConfig.java
@@ -0,0 +1,14 @@
+package com.fr.momozer.userJTT.userjtt.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration @EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+
+    }
+}
